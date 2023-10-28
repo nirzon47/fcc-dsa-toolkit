66087ebe800b931d5ab858f4d7f9ed4cc172f36f
Nirzon Karmakar <nirzon47@gmail.com>
2023-10-28 19:55:58 +0530

    general UI structured

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['*.html', 'pages/*.html'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['dracula'],
+	},
+}
